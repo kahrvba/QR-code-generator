e2ce236a22281f49ea71862dc06f3b7617272166
Kahrvba <ahmedsalah998.as@gmail.com>
2024-04-27 02:13:30 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+#install all libaries needed
+#create a funtion that collect a text and convert to qr-code
+#save qr-code as image
+#run the function
+
+import qrcode
+
+def generate_qrcode(text):
+    
+    qr = qrcode.QRCode(
+        version = 1,
+        error_correction = qrcode.constants.ERROR_CORRECT_L,
+        box_size =10,
+        border=4,
+    )
+
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("qrimg001.png")
+
+url = input("Enter your url: ")
+generate_qrcode(url)
